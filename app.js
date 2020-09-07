157a4124d43b9092ace035ae38e7c02256351afd
anuganesh44 <44762698+anuganesh44@users.noreply.github.com>
2020-09-07 21:59:19 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,52 @@
+document.getElementById('name').addEventListener('blur', validateName);
+document.getElementById('zip').addEventListener('blur', validateZipcode);
+document.getElementById('email').addEventListener('blur', validateEmail);
+document.getElementById('phone').addEventListener('blur', validatePhone);
+
+function validateName() {
+    const name = document.getElementById('name');
+    const re = /^[a-zA-Z]{2,10}$/;
+
+    if (!re.test(name.value)) {
+        name.classList.add('is-invalid');
+    } else {
+        name.classList.add('correct');
+        name.classList.remove('is-invalid');
+    }
+}
+
+function validateZipcode() {
+    const zip = document.getElementById('zip');
+    const re = /^[0-9]{5}$/;
+
+    if (!re.test(zip.value)) {
+        zip.classList.add('is-invalid');
+    } else {
+        zip.classList.add('correct');
+        zip.classList.remove('is-invalid');
+    }
+}
+
+function validateEmail() {
+    const email = document.getElementById('email');
+    const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+
+    if (!re.test(email.value)) {
+        email.classList.add('is-invalid');
+    } else {
+        email.classList.add('correct');
+        email.classList.remove('is-invalid');
+    }
+}
+
+function validatePhone() {
+    const phone = document.getElementById('phone');
+    const re = /^(\(\+[0-9]{3}\))?[0-9]{10}$/;
+
+    if (!re.test(phone.value)) {
+        phone.classList.add('is-invalid');
+    } else {
+        phone.classList.add('correct');
+        phone.classList.remove('is-invalid');
+    }
+}
